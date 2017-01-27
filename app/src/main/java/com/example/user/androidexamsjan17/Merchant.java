1029f32790ce681ee0423e4a803ade2b145accd6
Doberjohn <it1388@uom.edu.gr>
2017-01-27 15:05:25 +0200

    setup commit

---
+++ app/src/main/java/com/example/user/androidexamsjan17/Merchant.java
@@ -0,0 +1,16 @@
+package com.example.user.androidexamsjan17;
+
+/**
+ * Created by fanidis on 27/1/2017.
+ */
+
+public class Merchant {
+
+    private String id;
+    private String legalName;
+    private String category;
+    private String address;
+    private String imageUrl;
+    private String review;
+
+}
